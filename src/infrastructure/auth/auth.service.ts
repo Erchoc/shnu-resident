1843cdcec905357dd9a81c08075a003ffb3574b3
ThrowableFactory <ThrowableFactory@foxmail.com>
2018-06-30 00:03:05 +0800

    login

---
+++ src/infrastructure/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+    // constructor(private readonly usersService: UsersService) {}
+
+    async validateUser(token: string): Promise<any> {
+        // return await this.usersService.findOneByToken(token);
+        return await {username: 'admin', password: '12345'};
+    }
+}
